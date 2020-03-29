e442b9305365787f77650801d80db3dc8ca862ce
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-29 15:04:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,5 +8,8 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end 
+  end
 end
+
+def age_appropriate_birthday(birthday_kids)
+end 
